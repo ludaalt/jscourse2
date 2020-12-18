6fcb856d6c9524aa53c3a0c78bbabc2cee8d04b8
Luda <luda.tamb@mail.ru>
2020-12-18 18:28:27 +0300

    lesson 33

--- js/script.js
+++ js/script.js
@@ -1,58 +1,85 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Титаник",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против всех"
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const promoAdv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
+    const movieDB = {
+        movies: [
+            "Титаник",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против всех"
+        ]
+    };
 
-promoAdv.forEach(item => {
-    item.remove();
+    const promoAdv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if(favorite) {
+                console.log('Добавляем любимый фильм');
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        addForm.reset(); // event.target.reset();
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+    const makeChanges = () => {
+        genre.textContent = 'Драма';
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+        
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+    
+    const createMovieList = (films, parent) => {
+        parent.innerHTML = "";
+        sortArr(films);
+        
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1}. ${film}
+                    <div class="delete"></div>
+                </li>    
+            `
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', function(event) {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(films, parent);
+            });
+            
+        });        
+    };
+    deleteAdv(promoAdv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 });
-
-genre.textContent = 'Драма';
-
-poster.style.backgroundImage = "url('img/bg.jpg')";
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort(); 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${film}
-            <div class="delete"></div>
-        </li>    
-    `
-});
-
-
-/*const films = document.querySelectorAll('.promo__interactive-item');
-for (let i = 0; i < movieDB.movies.length; i += 1) {
-     
-    films[i].innerHTML = `${i + 1}. ${movieDB.movies[i]}`; 
-
-}
-*/
